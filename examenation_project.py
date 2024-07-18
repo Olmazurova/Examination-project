0a0e1b1cf67b0cc79a74653236806ca689dfb073
Ольга <Vsilena@mail.ru>
2024-07-18 15:25:33 +0300

    Добавлена возможность менять количество лучей звезды, нужно потестить в симуляторе. Исправления в комментариях

--- examenation_project.py
+++ examenation_project.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-''' Программируем дрон для полёта по траектории пятиконечной звезды '''
+''' Программирование дрона для полёта по траектории пятиконечной звезды '''
 
 # Библиотека __future__ нужна для обратной совместимости между python2 и python3
 from __future__ import print_function
@@ -15,7 +15,7 @@ import time
 from dronekit import connect, VehicleMode, LocationGlobalRelative
 
 
-# Получение строки подключения
+# Настройка параметров для получения строки подключения
 import argparse
 parser = argparse.ArgumentParser(description='Commands vehicle using vehicle.simple_goto.')
 parser.add_argument('--connect',
@@ -46,7 +46,7 @@ def arm_and_takeoff(aTargetAltitude):
     print("Основные проверки перед запуском")
     # Проверка готовности автопилота перед запуском
     while not vehicle.is_armable:
-        print(" Ожидание инициализации дрона...")
+        print("Ожидание инициализации дрона...")
         time.sleep(1)
 
     print("Включение двигателей")
@@ -88,9 +88,12 @@ angle = math.pi / 2  # угол 90 градусов
 radius = 0.002 # радиус для широты
 coefficient = 111.11 / (111.3 * math.cos(lat_h)) # коэффициент корректировки радиуса для долготы
 
+# Задаём количество лучей звезды
+rays = 5
+
 # Дрон облетает месность по траектории звезды
 # Через цикл вычисляем координаты для каждой точки
-for i in range(11):
+for i in range(rays * 2 + 1):
     if i % 2 != 0:
         x = round(long_h - radius * coefficient * 0.4 * math.cos(angle), 6)
         y = round(lat_h + radius * 0.4 * math.sin(angle), 6)
@@ -101,7 +104,7 @@ for i in range(11):
         x = round(long_h - radius * coefficient * math.cos(angle), 6)
         print(f"Полёт дрона к точке с координатами х: {x}, у: {y} на высоте 25 м.")
         vehicle.simple_goto(LocationGlobalRelative(y, x, 25))
-    angle += math.pi / 5 # если поставить знак минус, то дрон будет лететь против часовой стрелки 
+    angle += math.pi / rays # если поставить знак минус, то дрон будет лететь против часовой стрелки
                          # (если изменить число 5, то будет другое количество лучей?)
 
     # Приостанавливаем программу и смотрим движение дрона на карте
